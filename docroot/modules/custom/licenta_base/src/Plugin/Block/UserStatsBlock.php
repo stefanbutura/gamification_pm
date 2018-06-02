f475d5c92673e498e8a3b17196074c126939f7b2
stefanbutura <stefanbutura9@gmail.com>
2018-06-02 15:27:20 +0300

    user page.

---
+++ docroot/modules/custom/licenta_base/src/Plugin/Block/UserStatsBlock.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\licenta_base\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\TitleBlockPluginInterface;
+use Drupal\licenta_base\LicentaHelper;
+use Drupal\user\Entity\Role;
+use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
+
+/**
+ * Provides a block to display the user title.
+ *
+ * @Block(
+ *   id = "licenta_base_user_stats_block",
+ *   admin_label = @Translation("User stats"),
+ * )
+ */
+class UserStatsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $request = \Drupal::request();
+
+    $user = \Drupal::routeMatch()->getParameter('user');
+    if (!$user instanceof UserInterface) {
+      return [];
+    }
+
+    $level = $user->field_level->value;
+    $tasks = \Drupal::entityQuery('node')
+      ->condition('type', 'task')
+      ->condition('status', 1)
+      ->condition('field_status', LicentaHelper::$STATUS_APPROVED)
+      ->condition('field_assignee.entity.uid', $user->id())
+      ->execute();
+
+    $task_count = count($tasks);
+
+    $total_time = 0;
+    $spent_time = \Drupal::entityQuery('node')
+      ->condition('type', 'spent_time')
+      ->condition('field_assignee.entity.uid', $user->id())
+      ->execute();
+
+    foreach ($spent_time as $node) {
+      $total_time += $node->field_spent_time->value;
+    }
+
+    return [
+      '#theme' => 'licenta_base_user_stats_block',
+      '#spent_time' => $total_time,
+      '#task_count' => $task_count,
+    ];
+  }
+
+}
+
---
+++ docroot/modules/custom/licenta_base/src/Plugin/Block/UserTitleBlock.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\licenta_base\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\TitleBlockPluginInterface;
+use Drupal\user\Entity\Role;
+use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
+
+/**
+ * Provides a block to display the user title.
+ *
+ * @Block(
+ *   id = "licenta_base_user_title_block",
+ *   admin_label = @Translation("User title"),
+ * )
+ */
+class UserTitleBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $request = \Drupal::request();
+    $title = '';
+
+    $user = \Drupal::routeMatch()->getParameter('user');
+    if (!$user instanceof UserInterface) {
+      return [];
+    }
+
+    /** @var User $user */
+    $complete_title = $user->getAccountName();
+    $complete_title .= ', ' . $user->field_primary_title->entity->getTitle();
+    $complete_title .= ' ' . $user->field_secondary_title->entity->getTitle();
+
+    $picture = $user->user_picture;
+    $user_class = $user->getRoles();
+    $role = end($user_class);
+    $role_label = Role::load($role)->label();
+
+    return [
+      '#theme' => 'licenta_base_user_title_block',
+      '#title' => $complete_title,
+      '#role' => $role_label,
+      '#picture' => $picture,
+    ];
+  }
+
+}
+
