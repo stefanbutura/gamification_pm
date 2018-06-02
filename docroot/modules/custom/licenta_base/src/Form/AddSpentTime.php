b370efd77658a64b456d0519b60ce16e87b7d90a
stefanbutura <stefanbutura9@gmail.com>
2018-06-02 23:25:20 +0300

    mega progress.

---
+++ docroot/modules/custom/licenta_base/src/Form/AddSpentTime.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\licenta_base\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+
+class AddSpentTime extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'licenta_base_add_spent_time_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state, $task_number = NULL) {
+    $tasks = \Drupal::entityQuery('node')->condition('type', 'task')->condition('field_task_number', $task_number)->execute();
+    if (!count($tasks)) {
+      return;
+    }
+
+    $node = Node::load(reset($tasks));
+
+    $form['task_id'] = array(
+      '#type' => 'value',
+      '#value' => $node->id(),
+    );
+
+    $form['spent_time'] = array(
+      '#type' => 'number',
+      '#title' => t('Spent time:'),
+      '#required' => TRUE,
+    );
+
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!is_numeric($form_state->getValue('spent_time'))) {
+      $form_state->setErrorByName('spent_time', $this->t('Spent time value must be a number'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state, NodeInterface $node = NULL) {
+    $task_id = $form_state->getValue('task_id');
+    $user_id = \Drupal::currentUser()->id();
+
+    $ts_node = Node::create([
+      'type' => 'spent_time',
+      'title' => \Drupal::currentUser()->getDisplayName() . date('Y-m-d'),
+      'field_assignee' => ['target_id' => $user_id],
+      'field_task' => ['target_id' => $task_id],
+    ])->save();
+
+    drupal_set_message('Successfully added spent time');
+  }
+}
